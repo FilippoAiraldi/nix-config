23d91fe51c10ee3582005fe2033899de783420a4
Filippo Airaldi <filippoairaldi@gmail.com>
2025-09-28 19:26:07 +0200

    removed files and avatar face

--- modules/nixos/common/default.nix
+++ modules/nixos/common/default.nix
@@ -88,21 +88,6 @@
     shell = pkgs.zsh;
   };
 
-  # Set User's avatar
-  system.activationScripts.script.text = ''
-    mkdir -p /var/lib/AccountsService/{icons,users}
-    cp ${userConfig.avatar} /var/lib/AccountsService/icons/${userConfig.name}
-
-    touch /var/lib/AccountsService/users/${userConfig.name}
-
-    if ! grep -q "^Icon=" /var/lib/AccountsService/users/${userConfig.name}; then
-      if ! grep -q "^\[User\]" /var/lib/AccountsService/users/${userConfig.name}; then
-        echo "[User]" >> /var/lib/AccountsService/users/${userConfig.name}
-      fi
-      echo "Icon=/var/lib/AccountsService/icons/${userConfig.name}" >> /var/lib/AccountsService/users/${userConfig.name}
-    fi
-  '';
-
   # Passwordless sudo
   security.sudo.wheelNeedsPassword = false;
 
