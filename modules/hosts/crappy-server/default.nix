51626579a6314bc210f07433aedbd8b34756ddfe
Filippo Airaldi <filippoairaldi@gmail.com>
2026-05-13 21:49:52 +0200

    (modules/nixos/services) openssh, tailscale, power saving, lid-close, nvidia gpu (disabled now)

--- modules/hosts/crappy-server/default.nix
+++ modules/hosts/crappy-server/default.nix
@@ -10,5 +10,20 @@
     primaryUser = "fairaldi";
 
     system.stateVersion = "26.05";
+
+    # # support for nvidia gpu. since it's old, also the kernel must be quite old (https://nixos.wiki/wiki/Nvidia)
+    # boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_12;
+    # hardware.graphics.enable = true;
+    # services.xserver.videoDrivers = [ "nvidia" ];
+    # hardware.nvidia = {
+    #   modesetting.enable = true;
+    #   open = false;
+    #   nvidiaSettings = true;
+    #   package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
+    # };
+    # nixpkgs.config = {
+    #   allowUnfree = true;
+    #   nvidia.acceptLicense = true;
+    # };
   };
 }
