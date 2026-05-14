e5254ccf7bdc55ec8ea6f9d9c9d2d51a3f320441
Filippo Airaldi <filippoairaldi@gmail.com>
2026-05-14 20:52:51 +0200

    (cleaning) removed scripts

--- modules/base.nix
+++ modules/base.nix
@@ -71,7 +71,6 @@ in
       homeManager.opentofu
       homeManager.packages
       homeManager.podman
-      homeManager.scripts
       homeManager.starship
       homeManager.tmux
       homeManager.xdg
